71de24a6fdc1fef2a9dfff86b0df5714a06ec0a8
Annam <abch@itu.dk>
2024-09-18 22:53:57 +0200

    added the two channels, and they are now talking with each other

--- Tcp/Main.go
+++ Tcp/Main.go
@@ -2,8 +2,53 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
-	fmt.Println("i am main")
+
+serverCh := make(chan int)
+
+clientCh := make(chan int)
+var wg sync.WaitGroup
+
+wg.Add(2) 
+
+
+
+//clientCh goroutine 
+go func() {
+
+//wating for the syn msg from the client-channel
+	defer wg.Done()
+	serverCh <- 100
+	synAck1 := <- clientCh
+	synAck2 := <- clientCh
+	fmt.Println(synAck1, synAck2)
+	
+	serverCh <- 301
+	serverCh <- 101
+
+	
+}()
+	 
+//serverCh goroutine 
+go func() {
+
+	defer wg.Done() 
+    syn := <-serverCh 
+	fmt.Println( syn )
+
+	
+	clientCh <- 101
+	clientCh <- 300
+	ack1:= <-serverCh 
+	ack2:= <- serverCh
+	fmt.Println( ack1, ack2 )
+
+	}() 
+		wg.Wait()
+
 }
+
+
