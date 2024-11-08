3c0086f892064b9e30f252bd2a07d05b10f1787c
Asger124 <asjo@itu.dk>
2024-11-08 12:37:33 +0100

    Made function startclient()

--- Mutual_Exclusion/Main.go
+++ Mutual_Exclusion/Main.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	pb "example.com/Mutual_Exclusion/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type peer struct {
@@ -17,6 +19,7 @@ type peer struct {
 	token_chan chan bool
 	server     *grpc.Server
 	serve      *pb.UnimplementedTokenRingServer
+	cient      pb.TokenRingClient
 }
 
 func createPeer(id int, addr string) *peer {
@@ -41,6 +44,24 @@ func (p *peer) StartServer() {
 		log.Fatalf("Failed to serve: %v", err)
 	}
 }
+func (p *peer) StartClient() {
+	var conn *grpc.ClientConn
+	//var err error
+
+	for {
+		_, err := grpc.Dial(p.p_next.address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+		if err == nil {
+			break
+		}
+		log.Printf("Peer %d: Failed to connect to peer %d at %s, retrying...", p.id, p.p_next.id, p.p_next.address)
+		time.Sleep(2 * time.Second)
+	}
+	p.cient = pb.NewTokenRingClient(conn)
+	log.Printf("Peer %d: Successfully connected to peer %d at %s", p.id, p.p_next.id, p.p_next.address)
+}
+
+func (p *peer) passToken()
 
 func main() {
 
@@ -66,18 +87,4 @@ func main() {
 
 	select {}
 
-	// conn1, err := grpc.Dial(addr1, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	// if err != nil {
-	// 	log.Fatalf("Did not connect: %v", err)
-	// }
-	// defer conn1.Close()
-
-	// client1 := pb.NewTimeServiceClient(conn1)
-
-	// addr2 := fmt.Sprintf("localhost:%s", *port2)
-
-	// conn2, err := grpc.Dial(addr2, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	// if err != nil {
-	// 	log.Fatalf("Did not connect: %v", err)
-	// }
 }
