daffa316a0dc61d163d0d6eacf42830e7cb008eb
JojoStarman <151638823+JojoStarman@users.noreply.github.com>
2024-11-08 15:03:43 +0100

    main still needs work

---
+++ TokenRing/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	pb "TokenRing/proto"
+	"google.golang.org/grpc"
+
+)
+
+type Node struct {
+	//pb.UnimplementedChittyChatServiceServer
+	//stream pb.ChittyChatService_ChatStreamServer
+	id     int32
+	
+}
+
+
